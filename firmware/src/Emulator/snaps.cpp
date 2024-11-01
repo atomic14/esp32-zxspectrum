dd4a53a16de81b1a68d392cba68e9a264b823c1d
Chris Greening <c_greening@apple.com>
2024-11-01 18:57:13 +0000

    Fix missing algorithm

--- firmware/src/Emulator/snaps.cpp
+++ firmware/src/Emulator/snaps.cpp
@@ -24,6 +24,7 @@
  ======================================================================*/
 #include <stdint.h>
 #include <stdio.h>
+#include <algorithm>
 #include <string>
 #include <string.h>
 #include "./z80/z80.h"
